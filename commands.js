3df30cc2aadbfa339e439f47345edd7c45cadba7
Prithaj Nath <prithajnath@gmail.com>
2020-07-02 02:45:41 +0000

    Play YouTube URLs instead of random shit

--- commands.js
+++ commands.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const currentDir = path.join('music');
 const mp3Files = [];
+const { exec } = require("child_process");
 
 
 fs.readdir(currentDir, (err, files) => {
@@ -14,22 +15,61 @@ fs.readdir(currentDir, (err, files) => {
 });
 
 
-function createRandomStream(){
-  console.log(mp3Files);
-  var fileIndex = Math.floor(Math.random()*mp3Files.length);
-  console.log(`Playing ${mp3Files[fileIndex]}...`)
-  return fs.createReadStream(`${currentDir}/${mp3Files[fileIndex]}`);
+var URLs = [];
+fs.readFile('urls.txt', 'utf8', function (err,data) {
+  if(data){
+    URLs = data.split("\n");
+  }
+
+});
+
+console.log(URLs);
+
+
+function createRandomStream(fileName){
+//  console.log(mp3Files);
+//  var fileIndex = Math.floor(Math.random()*mp3Files.length);
+//  console.log(`Playing ${mp3Files[fileIndex]}...`)
+  console.log(`Playing ${fileName}...`)
+  return fs.createReadStream(`music/${fileName}`);
+}
+
+
+async function downloadMP3(url){
+
+const fileName = url.split("v=")[1];
+await exec(`youtube-dl -x --audio-format mp3 -o music/${fileName}.mp3 ${url}`, (error, stdout, stderr) => {
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+		return;
+	}
+		console.log(`stdout: ${stdout}`);
+	        return;
+	});
+return `${fileName}.mp3`;
 }
 
 
 
 const commands = {
-  '/trashup': async (message) => {
+  '/trashup': async (url, message) => {
     if (message.member.voiceChannel) {
+      var fn = null;
       try{
+	if(URLs.indexOf(url) < 0){
+	  fn = await downloadMP3(url);
+	  URLs.push(url);
+	}else{
+	 name = url.split("v=")[1];
+	 fn = `${name}.mp3`
+	}
         let channelConnection = await message.member.voiceChannel.join();
         message.reply('Time to trash it up');
-        channelConnection.playStream(createRandomStream());
+        channelConnection.playStream(createRandomStream(fn));
       } catch(e){
         console.log(e);
       }
@@ -39,7 +79,7 @@ const commands = {
     }
   },
 
-  '/trashdown': async (message) => {
+  '/trashdown': async (url, message) => {
     if (message.member.voiceChannel){
          message.member.voiceChannel.leave();
       }
--- index.js
+++ index.js
@@ -6,8 +6,11 @@ client.login(process.env.BOT_TOKEN);
 client.on('message', async function(message){
   if (!message.guild) return;
   try{
-    if (Object.keys(commands).indexOf(message.content) >= 0){
-        await commands[message.content](message);
+    var [command, url]  = message.content.split(' ');
+    console.log(command);
+    console.log(url);
+    if (Object.keys(commands).indexOf(command) >= 0){
+        await commands[command](url, message);
     }
   } catch(e){
     console.log(e);
